24796c1f724677093ecb77472e825aaa65025769
Josi McClellan <josiahmcclellan07@gmail.com>
2018-03-24 15:05:06 -0600

    add meals routes

--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -1,21 +1,45 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-/* GET users listing. */
-router.get('/', function(req, res, _next) {
-  res.status(500).json({ error: 'meals index NYI' });
-});
+var environment = process.env.NODE_ENV || 'development'
+var configuration = require('../../../knexfile')[environment]
+var knex = require('knex')(configuration)
 
-router.get('/:meal_id/foods', function(req, res, _next) {
-  res.status(500).json({ error: 'meal foods index NYI' });
-});
+router.get('/', (req, res, _next) => (
+  knex('meals')
+    .select('meals.name AS mealName', 'meals.id AS mealId', 'foods.*')
+    .leftOuterJoin('meal_foods', 'meals.id', '=', 'meal_foods.mealId')
+    .innerJoin('foods', 'foods.id', 'meal_foods.foodId')
+    .then(foods => Object.values(foods.reduce((meals, food) => {
+      const { mealId: id, mealName: name } = food
+      delete food.mealId
+      delete food.mealName
+      if (!meals[id]) meals[id] = { id, name, foods: [] }
+      meals[id].foods.push(food)
+      return meals
+    }, {})))
+      .then(meals => res.status(200).json(meals))
+))
 
-router.post('/:meal_id/foods/:id', function(req, res, _next) {
-  res.status(500).json({ error: 'meal foods create NYI' });
-});
+router.get('/:mealId/foods', function(req, res, _next) {
+  knex('foods')
+    .select('foods.*')
+    .innerJoin('meal_foods', 'foods.id', 'meal_foods.foodId')
+    .where('meal_foods.mealId', req.params.mealId)
+    .then(foods => res.status(200).json(foods))
+})
 
-router.delete('/:meal_id/foods/:id', function(req, res, _next) {
-  res.status(500).json({ error: 'meals index NYI' });
-});
+router.post('/:mealId/foods/:foodId', function(req, res, _next) {
+  knex('meal_foods')
+    .insert(req.params)
+    .then(() => res.status(201).json({}))
+})
 
-module.exports = router;
+router.delete('/:mealId/foods/:foodId', function(req, res, _next) {
+  knex('meal_foods')
+    .where(req.params)
+    .del()
+    .then(() => res.status(204).end())
+})
+
+module.exports = router
