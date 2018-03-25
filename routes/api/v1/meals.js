83ee4c1e839a27db264c4486b6a689cc47b2b935
leepuppychow <leepuppychow@gmail.com>
2018-03-25 15:44:58 -0600

    Creates meal.js model

--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -1,16 +1,9 @@
 var express = require('express')
 var router = express.Router()
-
-var environment = process.env.NODE_ENV || 'development'
-var configuration = require('../../../knexfile')[environment]
-var knex = require('knex')(configuration)
+const Meal = require('../../../models/meal')
 
 router.get('/', (request, response) => {
-  knex('meals')
-    .select(['meals.id', 'meals.name', knex.raw('ARRAY_TO_JSON(ARRAY_AGG(foods.*)) AS foods')])
-    .innerJoin('meal_foods', 'meals.id', 'meal_foods.mealId')
-    .innerJoin('foods', 'meal_foods.foodId', 'foods.id')
-    .groupBy('meals.id')
+  Meal.all()
   .then(meals => {
     response.status(200).json(meals)
   })
@@ -18,12 +11,7 @@ router.get('/', (request, response) => {
 })
 
 router.get('/:meal_id/foods', (request, response) => {
-  knex('meals')
-    .select(['meals.id', 'meals.name', knex.raw('ARRAY_TO_JSON(ARRAY_AGG(foods.*)) AS foods')])
-    .innerJoin('meal_foods', 'meals.id', 'meal_foods.mealId')
-    .innerJoin('foods', 'meal_foods.foodId', 'foods.id')
-    .groupBy('meals.id')
-    .where('meals.id', request.params.meal_id)
+  Meal.show(request.params.meal_id)
   .then(meals => {
     if (meals.length) {
       response.status(200).json(meals)
@@ -36,17 +24,13 @@ router.get('/:meal_id/foods', (request, response) => {
 })
 
 router.post('/:mealId/foods/:foodId', (request, response) => {
-  knex('meal_foods')
-  // Note: request params is already an object like {mealId: 5, foodId: 6}
-    .insert(request.params)
+  Meal.create(request.params)
     .then(() => response.status(201).json(`Food with ID: ${request.params.foodId} successfully added to meal with ID: ${request.params.mealId}`))
     .catch((error) => response.status(422).json({ error }))
 })
 
 router.delete('/:mealId/foods/:foodId', (request, response) => {
-  knex('meal_foods')
-    .where(request.params)
-    .del()
+  Meal.destroy(request.params)
     .then(() => response.status(204).end())
 })
 
