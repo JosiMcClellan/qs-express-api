04650f4578ea64e2af22fa602bb6504551b86184
leepuppychow <leepuppychow@gmail.com>
2018-03-25 16:09:26 -0600

    Adds CORS capability to app.js; needs to be tested

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
 app.use('/api/v1/foods', foods)
 app.use('/api/v1/meals', meals)
 
